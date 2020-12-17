d4b8b41357efda3b9ed7c6f2b7e8b4ec3f6d5c8c
Karl-Edouard Glaser <karl-edouard.glaser@epitech.eu>
2020-12-17 17:30:52 +0100

    new folder for api related files

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ import math
 from poloniex import Poloniex
 import os
 
-import account
-import charts
-import trades
+import api_request.account
+import api_request.charts
+import api_request.trades
 
-obj_poloniex = account.log()
+obj_poloniex = api_request.account.log()
 
 # API-KEY IK93HX6R-1RLQR66Q-ZA68OZD0-D1ADEV51
 # SECRET 94a9667e060d10fc0cee29fe9b0e79ac7490acef8eb86a8105706beb6521757e40d57c033ca8d3f620010c5f539b442fb812fba2d24f4006454425c8132567a6
