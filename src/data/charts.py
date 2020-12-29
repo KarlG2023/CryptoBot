dff058d7a8642186fd4d1e9cc0f1503ffbe96429
mtom <tom.marescot@epitech.eu>
2020-12-29 18:24:55 +0100

    puting all value in json and keeping them by order

--- src/data/charts.py
+++ src/data/charts.py
@@ -24,5 +24,47 @@ class charts_json:
         self.data = str(api_request.charts.get_chart_data(self.poloniex_obj, ['USDT_BTC'], 900, start=int(time.time())-(86400*30), end=int(time.time()))).replace("'", '"')
         self.data2 = self.data.replace("None", "null")
 
+    def get_data(self):
+        data = []
+        new_data = []
+        tmp = str()
+        data_json = {}
+        data_json['candle'] = []
+
+        for i in range(1, len(self.data2) - 1):
+            if i > 2:
+                if self.data2[i] == ',' and self.data2[i - 1] == '}':
+                    data.append(tmp)
+                    tmp = str()
+                    i += 1
+            tmp += str(self.data2[i])
+        #with open('data/json/cryptocurrencies.json', 'w') as f:
+            #json.dump(self.data2, f)
+        for i in range(1, len(data)):
+            new_tmp = {}
+            data_row = []
+            tmp_data = str()
+            tmp_data = data[i].replace(" ", "").replace('"', "'").replace("{", "").replace("}", "")
+            tmp_data_data = str()
+            for i in tmp_data:
+                if i == ':':
+                    tmp_data_data += i + "'"
+                elif i == ',':
+                    tmp_data_data += "'" + i
+                else:
+                    tmp_data_data += i
+            tmp_data_data += "'"
+            data_row = tmp_data_data.split(',')
+            for i in data_row:
+                a = i.replace("'", "").split(':')
+                new_tmp[a[0]] = a[1]
+            data_json['candle'].append(new_tmp)
+        
+        with open("data/json/cryptocurrencies.json", "w") as outfile:
+            json.dump(data_json, outfile)
+
     def print_data(self):
-        print(self.data2)
+        with open("data/json/cryptocurrencies.json") as json_file:
+            data_json = json.load(json_file)
+            for i in data_json["candle"]:
+                print(i["date"])
