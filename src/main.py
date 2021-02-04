7265d8f6bf433fde5134c9aa19e6e7c8b9f51e43
Karl-Edouard <karl-edouard.glaser@epitech.eu>
2021-02-04 18:16:44 +0100

    final changes for test phase

--- src/main.py
+++ src/main.py
@@ -74,7 +74,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.widgets_update()
 
         # update balance (comment for test phase)
-        if int(time.strftime("%S")) == 0:
+        if int(time.strftime("%M")) == 0:
             param.balance = api_request.account.get_balance(param.poloniex_obj)
 
         period = ""
@@ -99,156 +99,162 @@ class MainWindow(QtWidgets.QMainWindow):
         if int(time.strftime(period))%duration == update_latency and int(time.strftime("%S")) == 0 and param.bot_status == 1:
             param.bot_status = 2
             if param.cryptobot.get_status("BTC") == chart_analysis.analysis.ACTION.BUY and param.balance['USDT'] != 0:
+                param.balance = api_request.account.get_balance(param.poloniex_obj)
                 price = api_request.charts.get_ticker(param.poloniex_obj)['USDT_BTC']['last']
                 quantity = (param.balance['USDT'] / price)*(pow(param.bull_strength['BTC'], 2))
                 if param.balance['USDT'] - (quantity * price) > 1:
                     param.bear_strength['BTC'] = 0.9
                     param.bull_strength['BTC'] += 0.1
-                    param.balance['BTC'] = param.balance['BTC'] + quantity
-                    param.balance['USDT'] = param.balance['USDT'] - (quantity * price)
+                    # param.balance['BTC'] = param.balance['BTC'] + quantity
+                    # param.balance['USDT'] = param.balance['USDT'] - (quantity * price)
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.btc_order)
-                    # param.btc_order = api_request.trades.buy(param.poloniex_obj, "USDT_BTC", price, quantity, 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.btc_order)
+                    param.btc_order = api_request.trades.buy(param.poloniex_obj, "USDT_BTC", price, quantity, 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("BOUGHT " + str(quantity) + " BTC at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
                 if param.balance['USDT'] - (quantity * price) < 1:
-                    param.balance['BTC'] = param.balance['BTC'] + (param.balance['USDT'] / price)
-                    param.balance['USDT'] = 0
+                    # param.balance['BTC'] = param.balance['BTC'] + (param.balance['USDT'] / price)
+                    # param.balance['USDT'] = 0
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.btc_order)
-                    # param.btc_order = api_request.trades.buy(param.poloniex_obj, "USDT_BTC", price, param.balance['USDT'], 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.btc_order)
+                    param.btc_order = api_request.trades.buy(param.poloniex_obj, "USDT_BTC", price, param.balance['USDT'], 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("BOUGHT " + str(param.balance['USDT'] / price) + " BTC at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
 
             if param.cryptobot.get_status("BTC") == chart_analysis.analysis.ACTION.SELL and param.balance['BTC'] != 0:
+                param.balance = api_request.account.get_balance(param.poloniex_obj)
                 price = api_request.charts.get_ticker(param.poloniex_obj)['USDT_BTC']['last']
                 quantity = (param.balance['BTC'])*(pow(param.bear_strength['BTC'], 2))
                 if (param.balance['BTC'] * price) - quantity * price > 1:
                     param.bull_strength['BTC'] = 0.1
                     param.bear_strength['BTC'] -= 0.1
-                    param.balance['USDT'] += price * quantity
-                    param.balance['BTC'] = param.balance['BTC'] - quantity
+                    # param.balance['USDT'] += price * quantity
+                    # param.balance['BTC'] = param.balance['BTC'] - quantity
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.btc_order)
-                    # param.btc_order = api_request.trades.sell(param.poloniex_obj, "USDT_BTC", price, quantity, 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.btc_order)
+                    param.btc_order = api_request.trades.sell(param.poloniex_obj, "USDT_BTC", price, quantity, 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("SOLD " + str(quantity) + " BTC at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
                 if (param.balance['BTC'] * price) - quantity * price < 1:
-                    param.balance['USDT'] += price * param.balance['BTC']
-                    param.balance['BTC'] = 0
+                    # param.balance['USDT'] += price * param.balance['BTC']
+                    # param.balance['BTC'] = 0
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.btc_order)
-                    # param.btc_order = api_request.trades.sell(param.poloniex_obj, "USDT_BTC", price, param.balance['BTC'], 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.btc_order)
+                    param.btc_order = api_request.trades.sell(param.poloniex_obj, "USDT_BTC", price, param.balance['BTC'], 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("SOLD " + str(quantity) + " BTC at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
 
             if param.cryptobot.get_status("ETH") == chart_analysis.analysis.ACTION.BUY and param.balance['USDT'] != 0:
+                param.balance = api_request.account.get_balance(param.poloniex_obj)
                 price = api_request.charts.get_ticker(param.poloniex_obj)['USDT_ETH']['last']
                 quantity = (param.balance['USDT'] / price)*(pow(param.bull_strength['ETH'], 2))
                 if param.balance['USDT'] - (quantity * price) > 1:
                     param.bear_strength['ETH'] = 0.9
                     param.bull_strength['ETH'] += 0.1
-                    param.balance['ETH'] = param.balance['ETH'] + quantity
-                    param.balance['USDT'] = param.balance['USDT'] - (quantity * price)
+                    # param.balance['ETH'] = param.balance['ETH'] + quantity
+                    # param.balance['USDT'] = param.balance['USDT'] - (quantity * price)
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.eth_order)
-                    # param.eth_order = api_request.trades.buy(param.poloniex_obj, "USDT_ETH", price, quantity, 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.eth_order)
+                    param.eth_order = api_request.trades.buy(param.poloniex_obj, "USDT_ETH", price, quantity, 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("BOUGHT " + str(quantity) + " ETH at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
                 if param.balance['USDT'] - (quantity * price) < 1:
-                    param.balance['ETH'] = param.balance['ETH'] + (param.balance['USDT'] / price)
-                    param.balance['USDT'] = 0
+                    # param.balance['ETH'] = param.balance['ETH'] + (param.balance['USDT'] / price)
+                    # param.balance['USDT'] = 0
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.eth_order)
-                    # param.eth_order = api_request.trades.buy(param.poloniex_obj, "USDT_ETH", price, param.balance['USDT'], 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.eth_order)
+                    param.eth_order = api_request.trades.buy(param.poloniex_obj, "USDT_ETH", price, param.balance['USDT'], 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("BOUGHT " + str(param.balance['USDT'] / price) + " ETH at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
 
             if param.cryptobot.get_status("ETH") == chart_analysis.analysis.ACTION.SELL and param.balance['ETH'] != 0:
+                param.balance = api_request.account.get_balance(param.poloniex_obj)
                 price = api_request.charts.get_ticker(param.poloniex_obj)['USDT_ETH']['last']
                 quantity = (param.balance['ETH'])*(pow(param.bear_strength['ETH'], 2))
                 if (param.balance['ETH'] * price) - quantity * price > 1:
                     param.bull_strength['ETH'] = 0.1
                     param.bear_strength['ETH'] -= 0.1
-                    param.balance['USDT'] += price * quantity
-                    param.balance['ETH'] = param.balance['ETH'] - quantity
+                    # param.balance['USDT'] += price * quantity
+                    # param.balance['ETH'] = param.balance['ETH'] - quantity
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.eth_order)
-                    # param.eth_order = api_request.trades.sell(param.poloniex_obj, "USDT_ETH", price, quantity, 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.eth_order)
+                    param.eth_order = api_request.trades.sell(param.poloniex_obj, "USDT_ETH", price, quantity, 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("SOLD " + str(quantity) + " ETH at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
                 if (param.balance['ETH'] * price) - quantity * price < 1:
-                    param.balance['USDT'] += price * param.balance['ETH']
-                    param.balance['ETH'] = 0
+                    # param.balance['USDT'] += price * param.balance['ETH']
+                    # param.balance['ETH'] = 0
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.eth_order)
-                    # param.eth_order = api_request.trades.sell(param.poloniex_obj, "USDT_ETH", price, param.balance['ETH'], 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.eth_order)
+                    param.eth_order = api_request.trades.sell(param.poloniex_obj, "USDT_ETH", price, param.balance['ETH'], 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("SOLD ETH at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
 
             if param.cryptobot.get_status("LTC") == chart_analysis.analysis.ACTION.BUY and param.balance['USDT'] != 0:
+                param.balance = api_request.account.get_balance(param.poloniex_obj)
                 price = api_request.charts.get_ticker(param.poloniex_obj)['USDT_LTC']['last']
                 quantity = (param.balance['USDT'] / price)*(pow(param.bull_strength['LTC'], 2))
                 if param.balance['USDT'] - (quantity * price) > 1:
                     param.bear_strength['LTC'] = 0.9
                     param.bull_strength['LTC'] += 0.1
-                    param.balance['LTC'] = param.balance['LTC'] + quantity
-                    param.balance['USDT'] = param.balance['USDT'] - (quantity * price)
+                    # param.balance['LTC'] = param.balance['LTC'] + quantity
+                    # param.balance['USDT'] = param.balance['USDT'] - (quantity * price)
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.ltc_order)
-                    # param.ltc_order = api_request.trades.buy(param.poloniex_obj, "USDT_LTC", price, quantity, 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.ltc_order)
+                    param.ltc_order = api_request.trades.buy(param.poloniex_obj, "USDT_LTC", price, quantity, 0, 0, 0)
                     
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("BOUGHT " + str(quantity) + " LTC at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
                 if param.balance['USDT'] - (quantity * price) < 1:
-                    param.balance['LTC'] = param.balance['LTC'] + (param.balance['USDT'] / price)
-                    param.balance['USDT'] = 0
+                    # param.balance['LTC'] = param.balance['LTC'] + (param.balance['USDT'] / price)
+                    # param.balance['USDT'] = 0
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.ltc_order)
-                    # param.ltc_order = api_request.trades.buy(param.poloniex_obj, "USDT_LTC", price, param.balance['USDT'], 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.ltc_order)
+                    param.ltc_order = api_request.trades.buy(param.poloniex_obj, "USDT_LTC", price, param.balance['USDT'], 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("BOUGHT " + str(param.balance['USDT'] / price) + " LTC at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
 
             if param.cryptobot.get_status("LTC") == chart_analysis.analysis.ACTION.SELL and param.balance['LTC'] != 0:
+                param.balance = api_request.account.get_balance(param.poloniex_obj)
                 price = api_request.charts.get_ticker(param.poloniex_obj)['USDT_LTC']['last']
                 quantity = (param.balance['LTC'])*(pow(param.bear_strength['LTC'], 2))
                 if (param.balance['LTC'] * price) - quantity * price > 1:
                     param.bull_strength['LTC'] = 0.1
                     param.bear_strength['LTC'] -= 0.1
-                    param.balance['USDT'] += price * quantity
-                    param.balance['LTC'] = param.balance['LTC'] - quantity
+                    # param.balance['USDT'] += price * quantity
+                    # param.balance['LTC'] = param.balance['LTC'] - quantity
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.ltc_order)
-                    # param.ltc_order = api_request.trades.sell(param.poloniex_obj, "USDT_LTC", price, quantity, 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.ltc_order)
+                    param.ltc_order = api_request.trades.sell(param.poloniex_obj, "USDT_LTC", price, quantity, 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("SOLD " + str(quantity) + " LTC at " + str(price))
                     print("Balance: " + str(param.balance) + "\n")
                 if (param.balance['LTC'] * price) - quantity * price < 1:
-                    param.balance['USDT'] += price * param.balance['LTC']
-                    param.balance['LTC'] = 0
+                    # param.balance['USDT'] += price * param.balance['LTC']
+                    # param.balance['LTC'] = 0
 
-                    # api_request.trades.cancelOrder(param.poloniex_obj, param.ltc_order)
-                    # param.ltc_order = api_request.trades.sell(param.poloniex_obj, "USDT_LTC", price, param.balance['LTC'], 0, 0, 0)
+                    api_request.trades.cancelOrder(param.poloniex_obj, param.ltc_order)
+                    param.ltc_order = api_request.trades.sell(param.poloniex_obj, "USDT_LTC", price, param.balance['LTC'], 0, 0, 0)
 
                     print("[" + str(time.strftime("%H")) + ":" + str(time.strftime("%M")) + ":" + str(time.strftime("%S")) + "]")
                     print("SOLD LTC at " + str(price))
