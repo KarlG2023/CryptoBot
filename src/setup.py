ba45f5107388e5f8b3e4339e85de037764ff5a0e
Karl-Edouard Glaser <karl-edouard.glaser@epitech.eu>
2021-02-09 22:04:14 +0100

    add setup.py

---
+++ src/setup.py
@@ -0,0 +1,37 @@
+import sys
+import poloniex
+import PySide2
+from cx_Freeze import setup, Executable
+
+import api_request.account
+import api_request.charts
+import api_request.trades
+
+import chart_analysis.analysis
+import chart_analysis.moving_averages
+import chart_analysis.oscillators
+
+import data.charts
+import data.currencies
+
+import widgets.cryptos
+import widgets.dashboard
+import widgets.login
+import widgets.parameters
+
+import param
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for
+# a console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "CryptoBot",
+        version = "0.1",
+        description = "A great trading app",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("main.py", base=base)])
